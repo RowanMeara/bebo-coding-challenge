55fe107bd3c50aefc5b5bc7a2cf93910161d83ab
RowanMeara <rrm89@cornell.edu>
2017-11-13 13:39:38 -0800

    Working video/audio stream.  Now on to microphone volume adjustment.

--- assets/entrypoints/index.js
+++ assets/entrypoints/index.js
@@ -1,3 +1,13 @@
 import '../style/style.sass'
 
 console.log('Hello')
+navigator.mediaDevices.getUserMedia({audio: true, video: true})
+.then( (mediaStream) => {
+  let video = document.querySelector('video')
+  video.src = window.URL.createObjectURL(mediaStream)
+  video.onloadedmetadata = () => {
+    video.play()
+  }
+}).catch( (err) => {
+  console.log(err.name + ": " + err.message)
+})
